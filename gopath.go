80f7c97649348fb7d4787251d480ad9386fe527e
Moises P. Sena <moisespsena@gmail.com>
2019-04-04 09:16:49 -0300

    Update Dependencies

--- gopath.go
+++ gopath.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/moisespsena/go-error-wrap"
+	"github.com/moisespsena-go/error-wrap"
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/phayes/permbits"
